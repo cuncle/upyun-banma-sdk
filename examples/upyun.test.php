ef26371088c77c7f7c61a6b821b10d62221bc308
cuncle <uncle5@outlook.com>
2018-01-25 14:17:53 +0800

    Create upyun.test.php

---
+++ examples/upyun.test.php
@@ -0,0 +1,20 @@
+<?php
+require_once('../vendor/autoload.php');
+require_once('../src/upyun.class.php');
+// 初始化 UpYun
+$upyun = new UpYun ('ClientKey', 'ClientSecret','/image/url/check');//参数获取可以参考文档：http://docs.upyun.com/ai/audit/
+
+// 设置请求任务
+$data = array(
+    'url' => 'http://demo.b0.upaiyun.com/demo.png',        // 鉴黄的URL
+    'notify_url' => 'http://notify.http.com/post',      //回调通知地址
+);
+// 调用拉取函数
+try {
+    //返回对应的任务ids
+    $ids = $upyun->request($data);
+    print $ids;
+} catch(Exception $e) {
+    echo $e->getCode();     // 错误代码
+    echo $e->getMessage();  // 具体错误信息
+}
